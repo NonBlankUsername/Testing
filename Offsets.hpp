e6ae0d2f54bac7d571388a323542dbb3bc4b0682
NonBlankUsername <iamacetwo@gmail.com>
2025-03-16 17:45:42 +0100

    Add files via upload

---
+++ Offsets.hpp
@@ -0,0 +1,15 @@
+
+#ifndef OFFSETS_HPP
+#define OFFSETS_HPP
+
+namespace Offsets {
+    const uintptr_t FakeDataModel = 0x612FDA8;
+    const uintptr_t DataModel = 0x1B8;
+    const uintptr_t Workspace = 0x158;
+    const uintptr_t Name = 0x70;
+    const uintptr_t Children = 0x78;
+    const uintptr_t ClassName = 0x8;
+    const uintptr_t Parent = 0x50;
+}
+
+#endif
